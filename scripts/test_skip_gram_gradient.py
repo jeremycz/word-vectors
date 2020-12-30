625473055d33b2b195d384a5c113e95a0e9bdb4b
jeremycz <jeremy.cz@gmail.com>
2020-12-30 13:06:17 +1300

    Implement analytical gradient calculation

--- scripts/test_skip_gram_gradient.py
+++ scripts/test_skip_gram_gradient.py
@@ -53,10 +53,10 @@ def cost(batch: np.array, embeddings: np.array, debug: bool = False) -> np.float
     for sample in batch:
         for ind, token in enumerate(sample):
             if ind != center_token_ind:
-            cost += np.log(softmax(
-                embeddings[vocab_size + sample[center_token_ind]],
-                embeddings[:vocab_size],
-                token))
+                cost += np.log(softmax(
+                    embeddings[vocab_size + sample[center_token_ind]],
+                    embeddings[:vocab_size],
+                    token))
 
     cost *= -1. / batch_size
     return cost
@@ -76,6 +76,61 @@ def gradient_fd(batch: np.array, embeddings: np.array, step_size: float = 1e-6) 
     return gradient
 
 
+def gradient_analytical(batch: np.array, embeddings: np.array, debug: bool = False) -> np.array:
+    """"""
+    #
+    if len(batch.shape) == 1:
+        batch = np.expand_dims(batch, axis=0)
+    elif len(batch.shape) > 2:
+        raise ValueError("Tokens array should not have more than 2 dimensions")
+    
+    batch_size = batch.shape[0]
+    if debug:
+        print(f"Batch size: {batch_size}")
+
+    #
+    n_tokens_per_sample = batch.shape[1]
+    if n_tokens_per_sample % 2 == 0:
+        raise ValueError("Sample size should be an odd number")
+
+    if debug:
+        print(f"Tokens per sample: {n_tokens_per_sample}")
+
+    center_token_ind = n_tokens_per_sample // 2
+    if debug:
+        print(f"Center token ind: {center_token_ind}")
+
+    #
+    if embeddings.shape[0] % 2 != 0:
+        raise ValueError("The first dimension of the embeddings array should be an even number")
+    
+    vocab_size = embeddings.shape[0] // 2
+    if debug:
+        print(f"Vocab size: {vocab_size}")
+
+    # Initialise matrix to store result
+    gradient = np.zeros(embeddings.shape, dtype=np.float64)
+
+    for sample in batch:
+        p_w_c = softmax(embeddings[vocab_size + sample[center_token_ind]], embeddings[:vocab_size], list(range(vocab_size)))
+        p_w_c = np.expand_dims(p_w_c, axis=1)
+
+        for ind, token in enumerate(sample):
+            if ind != center_token_ind:
+                # d/dvc
+                gradient[vocab_size + sample[center_token_ind]] += embeddings[token] - np.sum(p_w_c * embeddings[:vocab_size], axis=0)
+
+                # d/duw
+                gradient[:vocab_size] -= p_w_c * np.expand_dims(embeddings[vocab_size + sample[center_token_ind]], axis=0)
+
+                # d/duo
+                gradient[token] += embeddings[vocab_size + sample[center_token_ind]]
+
+    #
+    gradient *= -1. / batch_size
+    return gradient
+
+
 def test_skip_gram_cost_function() -> None:
     print("=== Test skip-gram cost function === ")
 
@@ -88,13 +143,13 @@ def test_skip_gram_cost_function() -> None:
     )
 
     batch0 = np.array([1,0,0], dtype=np.int64)
-    print(f"Cost: {cost(batch0, embeddings, True):.6f}")
+    print(f"Cost: {cost(batch0, embeddings, debug=True):.6f}")
     print(f"Expected cost: {2.253856}")
 
     print()
 
     batch1 = np.array([[1,0,0], [0,1,0]], dtype=np.int64)
-    print(f"Cost: {cost(batch1, embeddings, True):.6f}")
+    print(f"Cost: {cost(batch1, embeddings, debug=True):.6f}")
     print(f"Expected cost: {1.145078}")
 
 
@@ -111,9 +166,15 @@ def test_skip_gram_gradient() -> None:
 
     batch0 = np.array([1,0,0], dtype=np.int64)
 
-    # Compute gradient using finite difference
+    # Compare analytical gradient with finite-differencing result
     print(gradient_fd(batch0, embeddings, step_size=1e-6))
-    
+    print(gradient_analytical(batch0, embeddings, debug=False))
+
+    print()
+
+    batch1 = np.array([[1,0,0], [0,1,0]], dtype=np.int64)
+    print(gradient_fd(batch1, embeddings, step_size=1e-6))
+    print(gradient_analytical(batch1, embeddings, debug=False))
 
 if __name__ == "__main__":
     test_skip_gram_cost_function()
