09e42920fb34c2ddcfcac03f1665fa2f3705d1c7
jeremycz <jeremy.cz@gmail.com>
2020-12-30 13:16:13 +1300

    Combine gradient and cost calculation into one function

--- scripts/test_skip_gram_gradient.py
+++ scripts/test_skip_gram_gradient.py
@@ -1,6 +1,6 @@
 """Test cost and gradient computation for skip-gram word vector model
 """
-from typing import List, Union
+from typing import List, Union, Tuple
 
 import numpy as np
 
@@ -12,7 +12,7 @@ def softmax(target: np.array, candidates: np.array, candidate_ind: Union[np.int6
     return np.exp(np.dot(candidates[candidate_ind], target)) / np.sum(np.exp(np.dot(candidates, target)))
 
 
-def cost(batch: np.array, embeddings: np.array, debug: bool = False) -> np.float64:
+def cost(batch: np.array, embeddings: np.array, compute_gradients: bool, debug: bool = False) -> Union[np.float64, Tuple[np.float64, np.array]]:
     """Compute skip-gram cost function
 
     embeddings = [[-u-], [-v-]]
@@ -50,7 +50,15 @@ def cost(batch: np.array, embeddings: np.array, debug: bool = False) -> np.float
 
     #
     cost = 0.0
+    gradient = np.zeros(embeddings.shape, dtype=np.float64)
+
     for sample in batch:
+        p_w_c = None
+        if compute_gradients:
+            # Probabilty distribution over all candidate words
+            p_w_c = softmax(embeddings[vocab_size + sample[center_token_ind]], embeddings[:vocab_size], list(range(vocab_size)))
+            p_w_c = np.expand_dims(p_w_c, axis=1)
+
         for ind, token in enumerate(sample):
             if ind != center_token_ind:
                 cost += np.log(softmax(
@@ -58,76 +66,36 @@ def cost(batch: np.array, embeddings: np.array, debug: bool = False) -> np.float
                     embeddings[:vocab_size],
                     token))
 
+                if compute_gradients and p_w_c is not None:
+                    # d/dvc
+                    gradient[vocab_size + sample[center_token_ind]] += embeddings[token] - np.sum(p_w_c * embeddings[:vocab_size], axis=0)
+
+                    # d/duw
+                    gradient[:vocab_size] -= p_w_c * np.expand_dims(embeddings[vocab_size + sample[center_token_ind]], axis=0)
+
+                    # d/duo
+                    gradient[token] += embeddings[vocab_size + sample[center_token_ind]]
+
     cost *= -1. / batch_size
+
+    if compute_gradients:
+        gradient *= -1. / batch_size
+        return cost, gradient
+
     return cost
 
 
 def gradient_fd(batch: np.array, embeddings: np.array, step_size: float = 1e-6) -> np.array:
     """Computes gradient of the skip-gram cost function using finite differencing"""
-    cost_init = cost(batch, embeddings)
+    cost_init = cost(batch, embeddings, compute_gradients=False)
 
     gradient = np.zeros(embeddings.shape, dtype=np.float64)
     for ind, _ in np.ndenumerate(embeddings):
         embeddings_copy = np.copy(embeddings)
         embeddings_copy[ind] += step_size
-        cost_update = cost(batch, embeddings_copy)
+        cost_update = cost(batch, embeddings_copy, compute_gradients=False)
         gradient[ind] = (cost_update - cost_init) / step_size
 
-    return gradient
-
-
-def gradient_analytical(batch: np.array, embeddings: np.array, debug: bool = False) -> np.array:
-    """"""
-    #
-    if len(batch.shape) == 1:
-        batch = np.expand_dims(batch, axis=0)
-    elif len(batch.shape) > 2:
-        raise ValueError("Tokens array should not have more than 2 dimensions")
-    
-    batch_size = batch.shape[0]
-    if debug:
-        print(f"Batch size: {batch_size}")
-
-    #
-    n_tokens_per_sample = batch.shape[1]
-    if n_tokens_per_sample % 2 == 0:
-        raise ValueError("Sample size should be an odd number")
-
-    if debug:
-        print(f"Tokens per sample: {n_tokens_per_sample}")
-
-    center_token_ind = n_tokens_per_sample // 2
-    if debug:
-        print(f"Center token ind: {center_token_ind}")
-
-    #
-    if embeddings.shape[0] % 2 != 0:
-        raise ValueError("The first dimension of the embeddings array should be an even number")
-    
-    vocab_size = embeddings.shape[0] // 2
-    if debug:
-        print(f"Vocab size: {vocab_size}")
-
-    # Initialise matrix to store result
-    gradient = np.zeros(embeddings.shape, dtype=np.float64)
-
-    for sample in batch:
-        p_w_c = softmax(embeddings[vocab_size + sample[center_token_ind]], embeddings[:vocab_size], list(range(vocab_size)))
-        p_w_c = np.expand_dims(p_w_c, axis=1)
-
-        for ind, token in enumerate(sample):
-            if ind != center_token_ind:
-                # d/dvc
-                gradient[vocab_size + sample[center_token_ind]] += embeddings[token] - np.sum(p_w_c * embeddings[:vocab_size], axis=0)
-
-                # d/duw
-                gradient[:vocab_size] -= p_w_c * np.expand_dims(embeddings[vocab_size + sample[center_token_ind]], axis=0)
-
-                # d/duo
-                gradient[token] += embeddings[vocab_size + sample[center_token_ind]]
-
-    #
-    gradient *= -1. / batch_size
     return gradient
 
 
@@ -143,39 +111,25 @@ def test_skip_gram_cost_function() -> None:
     )
 
     batch0 = np.array([1,0,0], dtype=np.int64)
-    print(f"Cost: {cost(batch0, embeddings, debug=True):.6f}")
+    cost_val, gradient = cost(batch0, embeddings, compute_gradients=True, debug=True)
+    print(f"Cost: {cost_val:.6f}")
     print(f"Expected cost: {2.253856}")
-
-    print()
-
-    batch1 = np.array([[1,0,0], [0,1,0]], dtype=np.int64)
-    print(f"Cost: {cost(batch1, embeddings, debug=True):.6f}")
-    print(f"Expected cost: {1.145078}")
-
-
-def test_skip_gram_gradient() -> None:
-    print("=== Test skip-gram gradient function === ")
-
-    embeddings = np.array([
-        [1,4,5,8],
-        [2,3,6,7],
-        [1,2,3,4],
-        [1,3,5,7]],
-        dtype=np.float64    
-    )
-
-    batch0 = np.array([1,0,0], dtype=np.int64)
-
-    # Compare analytical gradient with finite-differencing result
+    print("Finite difference gradient:")
     print(gradient_fd(batch0, embeddings, step_size=1e-6))
-    print(gradient_analytical(batch0, embeddings, debug=False))
+    print("Analytical gradient:")
+    print(gradient)
 
     print()
 
     batch1 = np.array([[1,0,0], [0,1,0]], dtype=np.int64)
+    cost_val, gradient = cost(batch1, embeddings, compute_gradients=True, debug=True)
+    print(f"Cost: {cost_val:.6f}")
+    print(f"Expected cost: {1.145078}")
+    print("Finite difference gradient:")
     print(gradient_fd(batch1, embeddings, step_size=1e-6))
-    print(gradient_analytical(batch1, embeddings, debug=False))
+    print("Analytical gradient:")
+    print(gradient)
+
 
 if __name__ == "__main__":
     test_skip_gram_cost_function()
-    test_skip_gram_gradient()
